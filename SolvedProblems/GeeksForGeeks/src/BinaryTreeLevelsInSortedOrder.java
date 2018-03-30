c578d717d2f36c2e7d296a4d178076e1ef971c8d
Loc Truong <loctvuit@gmail.com>
2018-03-30 14:39:30 +0700

    Add GeeksForGeeks

---
+++ SolvedProblems/GeeksForGeeks/src/BinaryTreeLevelsInSortedOrder.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class BinaryTreeLevelsInSortedOrder {
+
+	static void levelTraverse(Integer[] arr) {
+		int i = 0;
+		int k = 0;
+		int numNodes = (int) Math.pow(2, k);
+		SortedSet<Integer> ss = new TreeSet<>();
+
+		while (i < arr.length) {
+			int j = 1;
+			while (j <= numNodes) {
+				ss.add(arr[i]);
+				++i;
+				++j;
+				if (i >= arr.length)
+					break;
+			}
+			Iterator<Integer> ssIter = ss.iterator();
+			while(ssIter.hasNext())
+				System.out.print(ssIter.next() + " ");
+			ss.clear();
+			System.out.println();
+			numNodes = (int)Math.pow(2, ++k);
+			if (i >= arr.length)
+				break;
+			
+		}
+	}
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		int n;
+		Integer[] arr;
+		
+		for (int t = 1; t <= T; ++t) {
+			n = Integer.parseInt(br.readLine());
+			arr = Arrays.stream(br.readLine().trim().split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
+			levelTraverse(arr);
+		}
+	}
+
+}
---
+++ Utilities/src/BinaryTree.java
@@ -0,0 +1,113 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTree<T extends Comparable<T>> {
+
+	@SuppressWarnings("hiding")
+	private class TreeNode<T extends Comparable<T>> {
+		TreeNode<T> leftChild;
+		TreeNode<T> rightChild;
+		T data;
+
+		public TreeNode(T data) {
+			this.data = data;
+			this.leftChild = null;
+			this.rightChild = null;
+		}
+	}
+
+	public TreeNode<T> root;
+
+	public BinaryTree() {
+		root = null;
+	}
+
+	private void insertHelper(TreeNode<T> node, T data) {
+		// left insert
+		if (data.compareTo(node.data) < 0) {
+			if (node.leftChild == null)
+				node.leftChild = new TreeNode<T>(data);
+			else
+				insertHelper(node.leftChild, data);
+		}
+
+		// right insert
+		else {
+			if (node.rightChild == null)
+				node.rightChild = new TreeNode<T>(data);
+			else
+				insertHelper(node.rightChild, data);
+		}
+	}
+
+	private void inorderTraversalHelper(TreeNode<T> node) {
+		if (node != null) {
+			inorderTraversalHelper(node.leftChild);
+			System.out.println(node.data);
+			inorderTraversalHelper(node.rightChild);
+		}
+	}
+
+	private void preorderTraversalHelper(TreeNode<T> node) {
+		if (node != null) {
+			System.out.println(node.data);
+			preorderTraversalHelper(node.leftChild);
+			preorderTraversalHelper(node.rightChild);
+		}
+	}
+
+	private void postorderTraversalHelper(TreeNode<T> node) {
+		if (node != null) {
+			postorderTraversalHelper(node.leftChild);
+			postorderTraversalHelper(node.rightChild);
+			System.out.println(node.data);
+		}
+	}
+
+	private void levelorderTraversalHelper(TreeNode<T> node) {
+		if (node == null)
+			return;
+		List<TreeNode<T>> nodes = new ArrayList<>();
+		nodes.add(node);
+
+		while (true) {
+			List<TreeNode<T>> nextNodes = new ArrayList<>();
+			for (int i = 0; i < nodes.size(); ++i) {
+				System.out.print(nodes.get(i).data + " ");
+				if (nodes.get(i).leftChild != null && nodes.get(i).rightChild != null) {
+					nextNodes.add(nodes.get(i).leftChild);
+					nextNodes.add(nodes.get(i).rightChild);
+				}
+			}
+
+			System.out.println();
+			if (nextNodes.size() == 0)
+				break;
+			nodes = nextNodes;
+		}
+	}
+
+	public void levelorderTraversal() {
+		levelorderTraversalHelper(root);
+	}
+
+	public void preorderTraversal() {
+		preorderTraversalHelper(root);
+	}
+
+	public void inorderTraversal() {
+		inorderTraversalHelper(root);
+	}
+
+	public void postorderTraversal() {
+		postorderTraversalHelper(root);
+	}
+
+	public void insert(T data) {
+		if (root == null)
+			root = new TreeNode<T>(data);
+		else
+			insertHelper(root, data);
+	}
+
+}
